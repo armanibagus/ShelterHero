a363b4d3e0e0f3eef5cb4aee828d5e5b1c093697
armanibagus <79289139+armanibagus@users.noreply.github.com>
2022-04-06 23:17:15 +0800

    Update 06/04/2022

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class HealthCheckController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('verified');
+    }
     /**
      * Display a listing of the resource.
      *
